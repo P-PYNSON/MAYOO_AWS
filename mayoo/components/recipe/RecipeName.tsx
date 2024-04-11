bf82a3ad86055defb6f7345b25d51a1939538052
Velwitch <phil.pynson@gmail.com>
2024-04-11 16:02:12 +0200

    done

--- mayoo/components/recipe/RecipeName.tsx
+++ mayoo/components/recipe/RecipeName.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
+import RNFS from 'react-native-fs';
 
 interface RecipeNameProps {
   setName: (text: string) => void;
@@ -7,19 +17,53 @@ interface RecipeNameProps {
   setImage: (text: string) => void;
   data: {
     name: string | undefined;
-    category: string | undefined;
     image: string | undefined;
   };
 }
 
-export default function RecipeName({
-  setName,
-  setCategory,
-  setImage,
-  data,
-}: RecipeNameProps) {
+export default function RecipeName({setName, setImage, data}: RecipeNameProps) {
+  const handleImagePicker = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        cropping: true,
+        mediaType: 'photo',
+        includeBase64: true,
+      });
+
+      try {
+
+        if (data.image) {
+          //delete previous image from system for obvious optimisation reasons
+          await RNFS.unlink(data.image);
+          console.log(`File at ${data.image} has been deleted successfully.`);
+        }
+
+        setImage(image.path);
+      } catch (error) {
+        console.error(`Error deleting file at ${data.image}:`, error);
+      }
+    } catch (error) {
+      console.log('Error picking image:', error);
+    }
+  };
+
   return (
     <View style={styles.mainView}>
+      <TouchableOpacity
+        onPress={() => {
+          handleImagePicker();
+        }}
+        style={styles.imageView}>
+        {data.image && data.image != '' && (
+          <Image
+            style={styles.image}
+            source={{
+              uri: data.image,
+            }}></Image>
+        )}
+      </TouchableOpacity>
       <View style={styles.inputView}>
         <Text style={styles.text}>Recipe Name</Text>
         <TextInput
@@ -31,14 +75,39 @@ export default function RecipeName({
           }}
           placeholder=""></TextInput>
       </View>
-     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mainView: {width: '90%', gap: 20},
-  inputView: {display: 'flex', justifyContent: 'center', alignItems: 'center'},
+  mainView: {
+    width: '90%',
+    gap: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageView: {
+    width: '90%',
+    height: 200,
+    backgroundColor: 'gray',
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    resizeMode: 'cover',
+    borderRadius: 20,
+  },
+  inputView: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   text: {
     width: '50%',
     backgroundColor: 'rgba(255, 255, 255, 1)',
