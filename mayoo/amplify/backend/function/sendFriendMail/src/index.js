57766cbe4999a1f6606cb6269899a4167fc1486c
Velwitch <phil.pynson@gmail.com>
2024-05-17 16:51:38 +0200

    emails dones. Need new lambda for db mutations

--- mayoo/amplify/backend/function/sendFriendMail/src/index.js
+++ mayoo/amplify/backend/function/sendFriendMail/src/index.js
@@ -1,8 +1,23 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["smtpPass"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 const AWS = require('aws-sdk');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
 const cognito = new AWS.CognitoIdentityServiceProvider();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -50,6 +65,9 @@ exports.handler = async event => {
           },
         };
         await dynamodb.put(friendRequestParams).promise();
+
+        const emailSent = await sendEmail(token, id, targetEmail);
+        console.log(emailSent);
       }
 
       return {
@@ -79,25 +97,49 @@ exports.handler = async event => {
   function generateRandomToken(length) {
     return crypto.randomBytes(length).toString('hex');
   }
-};
 
-async function sendEmail(email, sub, name) {
-  const params = {
-    Destination: {
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `Hi ${name},\n\nClick the following link to perform the operation: https://YOUR_API_GATEWAY_ENDPOINT/YOUR_PATH/${sub}`,
+  async function sendEmail(token, requestId, email) {
+    try {
+      const ssm = new AWS.SSM();
+      console.log('sending');
+      const {Parameters} = await ssm
+        .getParameters({
+          Names: ['smtpPass'].map(secretName => process.env[secretName]),
+          WithDecryption: true,
+        })
+        .promise();
+      const smtPass = Parameters[0]['Value'];
+      console.log('smpt pass:' + smtPass);
+
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.mailersend.net',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'MS_uALO4J@trial-jpzkmgqyzd1l059v.mlsender.net',
+          pass: smtPass,
         },
-      },
-      Subject: {
-        Data: 'Perform Operation',
-      },
-    },
-    Source: 'YOUR_EMAIL_ADDRESS', // SES verified email address
-  };
+      });
 
-  await ses.sendEmail(params).promise();
-}
+      // Email options
+      let mailOptions = {
+        from: '"Mayoo App" <MS_uALO4J@trial-jpzkmgqyzd1l059v.mlsender.net>',
+        to: email,
+        subject: 'New friend request',
+        text: `Hello there ! ${senderName} wants to be your friend. Click on this link if you would like to add ${senderName} to your friends list and share recipes and lists together ! t:${token}  i:${requestId}`,
+        // html: '<b>Hello world?</b>' // HTML body (optional)
+      };
+
+      try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info.messageId);
+        return true;
+      } catch (error) {
+        console.error('Error sending email: %s', error);
+        return false;
+      }
+    } catch (error) {
+      console.log('error with email:' + error);
+    }
+  }
+};
